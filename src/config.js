6e2278fc651dd37ac3708235efa3b307f34fe3b5
ukarthik123 <53403086+ukarthik123@users.noreply.github.com>
2021-07-09 12:19:07 -0400

    Okta Integration on Klabs accounts

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export default {
   oidc: {
-    // clientId: '0oatjthnpG95NR1Na5d6',  // localhost:8080
-    clientId:'0oavpnyyg9sLED2bd5d6',    // Heroku preview app
-    issuer: 'https://dev-53342887.okta.com/oauth2/default',
+    // clientId: '0oa16g5fu4XzOkCvI5d7',  // localhost:8080
+    clientId:'0oa16fzkzjrmMHlMn5d7',    // Heroku production app
+    issuer: 'https://dev-56986132.okta.com/oauth2/default',
     redirectUri: window.location.origin + '/login/callback',
     scopes: ['openid', 'profile', 'email'],
   }
